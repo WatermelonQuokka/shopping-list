75b27bc095e86f1e42d198d1ffd2207b02bda1e7
WatermelonQuokka <danielchis123@hotmail.com>
2026-09-10 12:38:06 +0200

    added a menu with options and a if/else if chain with comments what they are supposed to do

--- shopping-list.cs
+++ shopping-list.cs
@@ -1,4 +1,35 @@
-
 List<string> product = [];
-
 List<int> price = [];
+
+while (true)
+{
+    string[] options = [
+        "Choose an option.",
+        "1. Add a product and price",
+        "2. Show your lists",
+        "3. Remove a product",
+        "9. Exit"
+    ];
+    
+    // adds a product and price to the lists
+    if(1)
+    {
+        
+    }
+    // shows the full lists with total price
+    else if(2)
+    {
+        
+    }
+    // removes both product and price from the lists
+    else if(3)
+    {
+        
+    }
+    // exits the loop
+    else if(9)
+    {
+        
+    }
+
+}
